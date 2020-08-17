65e49f3575a8f9d1078a250f74815f2d55800999
Polarpi <eugeniachen123@gmail.com>
2020-08-16 19:56:31 -0500

    add about page + section top

--- next-app/components/sectionhead.jsx
+++ next-app/components/sectionhead.jsx
@@ -1,9 +1,25 @@
-export default function SectionHead({ title }) {
-  return (
-    <div className="section-head">
-      <div className="section-title">
-        <h3>{title}</h3>
+function SectionTop({ children }) {
+  return <div className="section-top">{children}</div>;
+}
+
+export default function SectionHead({ title, top }) {
+  if (top) {
+    return (
+      <SectionTop>
+        <div className="section-head">
+          <div className="section-title">
+            <h3>{title}</h3>
+          </div>
+        </div>
+      </SectionTop>
+    );
+  } else {
+    return (
+      <div className="section-head">
+        <div className="section-title">
+          <h3>{title}</h3>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
