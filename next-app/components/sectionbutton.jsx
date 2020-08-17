362768f8ac48285d328b4e940532ad41d9470427
Polarpi <eugeniachen123@gmail.com>
2020-08-16 19:55:45 -0500

    update links

--- next-app/components/sectionbutton.jsx
+++ next-app/components/sectionbutton.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function SectionButton({ buttonText, buttonLink }) {
   return (
     <div className="button">
-      <a href={buttonLink} className="link-btn">
-        {buttonText}
-      </a>
+      <Link href={buttonLink}>
+        <a className="link-btn">{buttonText}</a>
+      </Link>
     </div>
   );
 }
