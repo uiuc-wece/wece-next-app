efe11b602427d7e8caf44ec27b3aed0e8132f8ef
Polarpi <eugeniachen123@gmail.com>
2020-08-16 19:56:02 -0500

    update links

--- next-app/pages/index.jsx
+++ next-app/pages/index.jsx
@@ -35,90 +35,89 @@ export default function Home() {
   });
   return (
     <Layout>
-      <div className={styles.container}>
-        <LinesSvg></LinesSvg>
-        <div className="content">
-          <Container fluid>
-            <div className={styles.title}>
-              <div className={styles["front-img"]}>
-                <Image
-                  fluid
-                  src="imgs/wece-logo-white.png"
-                  alt="wece-logo-white"
-                />
-              </div>
+      <LinesSvg></LinesSvg>
+      <div className="content">
+        <Container fluid>
+          <div className={styles.title}>
+            <div className={styles["front-img"]}>
+              <Image
+                fluid
+                src="imgs/wece-logo-white.png"
+                alt="wece-logo-white"
+              />
             </div>
-            <div className={styles.subheading}>
-              <h4 className={styles["wece-title"]}>
-                Women in Electrical and Computer Engineering
-              </h4>
-              <h6 className={styles["uiuc-subheading"]}>
-                University of Illinois Urbana-Champaign
-              </h6>
-            </div>
-          </Container>
-          <div className={styles["staff-pic-popup"]} id="staff-pic-popup">
-            <img
-              className={`${styles["img-center"]} ${styles["staff-pic"]}`}
-              id="staff-pic"
-              src="imgs/board2019-20.png"
-            />
           </div>
-          <Container fluid className="section">
-            <SectionHead title="About_"></SectionHead>
-            <SectionBody>
-              <p>
-                <span
-                  style={{
-                    color: "#2eb6ad",
-                    fontWeight: 600,
-                    fontFamily: "IBM Plex Mono",
-                  }}
-                >
-                  We are WECE
-                </span>{" "}
-                - a student organization at the University of Illinois at
-                Urbana-Champaign dedicated to supporting women within the
-                Electrical and Computer Engineering department. We welcome
-                members of all genders to be active in the group, and we strive
-                to build communities amongst ourselves by hosting social,
-                technical, outreach, and professional events.
-              </p>
-              <SectionButton
-                buttonText="Learn More"
-                buttonLink="about.html"
-              ></SectionButton>
-            </SectionBody>
-            <SectionHead title="Events_"></SectionHead>
-            <div class="section-body">
-              <EventCarousel></EventCarousel>
-            </div>
-            <SectionHead title="Committees_"></SectionHead>
-            <SectionBody>
-              <p>
-                WECE has SEVEN different committees you can join and be a part
-                of! Get involved with technical projects, mentor elementary
-                students, plan fun events, and more!
-              </p>
-              <SectionButton
-                buttonText="Learn More"
-                buttonLink="committees.html"
-              ></SectionButton>
-            </SectionBody>
-            <SectionHead title="Join Us_"></SectionHead>
-            <SectionBody>
-              <p>
-                If you are interested in learning more about WECE, fill out the
-                form below to get added to our mailing list! An email with
-                events and updates will be sent out every week.
-              </p>
-              <SectionButton
-                buttonText="Join us!"
-                buttonLink="join.html"
-              ></SectionButton>
-            </SectionBody>
-          </Container>
-          {/*<Container fluid className="section">
+          <div className={styles.subheading}>
+            <h4 className={styles["wece-title"]}>
+              Women in Electrical and Computer Engineering
+            </h4>
+            <h6 className={styles["uiuc-subheading"]}>
+              University of Illinois Urbana-Champaign
+            </h6>
+          </div>
+        </Container>
+        <div className={styles["staff-pic-popup"]} id="staff-pic-popup">
+          <img
+            className={`${styles["img-center"]} ${styles["staff-pic"]}`}
+            id="staff-pic"
+            src="imgs/board2019-20.png"
+          />
+        </div>
+        <Container fluid className="section">
+          <SectionHead title="About_" top={false}></SectionHead>
+          <SectionBody>
+            <p>
+              <span
+                style={{
+                  color: "#2eb6ad",
+                  fontWeight: 600,
+                  fontFamily: "IBM Plex Mono",
+                }}
+              >
+                We are WECE
+              </span>{" "}
+              - a student organization at the University of Illinois at
+              Urbana-Champaign dedicated to supporting women within the
+              Electrical and Computer Engineering department. We welcome members
+              of all genders to be active in the group, and we strive to build
+              communities amongst ourselves by hosting social, technical,
+              outreach, and professional events.
+            </p>
+            <SectionButton
+              buttonText="Learn More"
+              buttonLink="about"
+            ></SectionButton>
+          </SectionBody>
+          <SectionHead title="Events_" top={false}></SectionHead>
+          <SectionBody>
+            <EventCarousel></EventCarousel>
+          </SectionBody>
+          <SectionHead title="Committees_" top={false}></SectionHead>
+          <SectionBody>
+            <p>
+              WECE has SEVEN different committees you can join and be a part of!
+              Get involved with technical projects, mentor elementary students,
+              plan fun events, and more!
+            </p>
+            <SectionButton
+              buttonText="Learn More"
+              buttonLink="committees"
+            ></SectionButton>
+          </SectionBody>
+          <SectionHead title="Join Us_" top={false}></SectionHead>
+          <SectionBody>
+            <p>
+              If you are interested in learning more about WECE, fill out the
+              form below to get added to our mailing list! An email with events
+              and updates will be sent out every week.
+            </p>
+            <SectionButton
+              buttonText="Join us!"
+              buttonLink="join"
+            ></SectionButton>
+          </SectionBody>
+        </Container>
+        {/*<Container fluid className="section">
                 <div className={styles["section-head"]}>
                     <div className="section-title">
                     <h3 className={styles["text-center"]}>Upcoming Events_</h3>
@@ -131,7 +130,6 @@ export default function Home() {
                     </div>
                 </div>
             </Container>*/}
-        </div>
       </div>
     </Layout>
   );
