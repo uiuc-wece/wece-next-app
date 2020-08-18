df626eafdf06331f36f05c68ff15dbb61c5f8d60
Polarpi <eugeniachen123@gmail.com>
2020-08-18 16:41:36 -0500

    add hoverEffect and link option to SectionBody

--- next-app/components/sectionbody.jsx
+++ next-app/components/sectionbody.jsx
@@ -1,3 +1,43 @@
-export default function SectionBody({ children }) {
-  return <div className="section-body">{children}</div>;
+import styles from "../styles/Sectionbody.module.css";
+
+function SectionLink({ children, link = "" }) {
+  return (
+    <a className={styles["section-link"]} href={link} target="_blank">
+      {children}
+    </a>
+  );
+}
+
+export default function SectionBody({
+  children,
+  hoverEffect = false,
+  link = "",
+}) {
+  if (link != "") {
+    return (
+      <SectionLink children={children} link={link}>
+        <div
+          className={
+            hoverEffect
+              ? styles["section-body"] + " " + styles["section-hover-body"]
+              : styles["section-body"]
+          }
+        >
+          {children}
+        </div>
+      </SectionLink>
+    );
+  } else {
+    return (
+      <div
+        className={
+          hoverEffect
+            ? styles["section-body"] + " " + styles["section-hover-body"]
+            : styles["section-body"]
+        }
+      >
+        {children}
+      </div>
+    );
+  }
 }
