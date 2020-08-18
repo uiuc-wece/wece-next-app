4a8f61c7ce90e5b935bac0656ef181345b01212e
Polarpi <eugeniachen123@gmail.com>
2020-08-18 17:30:09 -0500

    use SectionBody on sponsors page

--- next-app/pages/sponsors.jsx
+++ next-app/pages/sponsors.jsx
@@ -1,4 +1,5 @@
 import Layout from "../components/layout";
+import SectionBody from "../components/sectionbody";
 import SectionHead from "../components/sectionhead";
 import styles from "../styles/Sponsors.module.css";
 
@@ -10,75 +11,39 @@ export default function Sponsors() {
       <div className="content">
         <Container fluid className="section">
           <SectionHead title="WECE Sponsor_" top={true} />
-          <a
-            className={styles["sponsor-link"]}
-            href="https://www.microsoft.com/en-us/about"
-            target="_blank"
+          <SectionBody
+            hoverEffect={true}
+            link="https://www.microsoft.com/en-us/about"
           >
-            <div className={styles["sponsor-button"]}>
-              <h1 className={styles["main-sponsor"]}>Microsoft</h1>
-            </div>
-          </a>
+            <h1 className={styles["main-sponsor"]}>Microsoft</h1>
+          </SectionBody>
           <SectionHead title="Donors_" top={true} />
           <h2 className={styles["sponsor-level"]}>Platinum level $2000+</h2>
-          <a
-            className={styles["sponsor-link"]}
-            href="https://www.chevron.com/technology/technology-ventures"
-            target="_blank"
+          <SectionBody
+            hoverEffect={true}
+            link="https://www.chevron.com/technology/technology-ventures"
           >
-            <div className={styles["sponsor-button"]}>
-              <h3 className={styles["donor-names"]}>
-                Chevron Technology Ventures
-              </h3>
-            </div>
-          </a>
+            <h3 className={styles["donor-names"]}>
+              Chevron Technology Ventures
+            </h3>
+          </SectionBody>
           <h2 className={styles["sponsor-level"]}>Gold level $1000+</h2>
-          <a
-            className={styles["sponsor-link"]}
-            href="https://www.bp.com/"
-            target="_blank"
-          >
-            <div className={styles["sponsor-button"]}>
-              <h3 className={styles["donor-names"]}>BP</h3>
-            </div>
-          </a>
-          <a
-            className={styles["sponsor-link"]}
-            href="https://www.micron.com/"
-            target="_blank"
-          >
-            <div className={styles["sponsor-button"]}>
-              <h3 className={styles["donor-names"]}>Micron</h3>
-            </div>
-          </a>
-          <a
-            className={styles["sponsor-link"]}
-            href="https://usa.visa.com/"
-            target="_blank"
-          >
-            <div className={styles["sponsor-button"]}>
-              <h3 className={styles["donor-names"]}>Visa</h3>
-            </div>
-          </a>
-          <a
-            className={styles["sponsor-link"]}
-            href="https://www.zillow.com/"
-            target="_blank"
-          >
-            <div className={styles["sponsor-button"]}>
-              <h3 className={styles["donor-names"]}>Zillow</h3>
-            </div>
-          </a>
+          <SectionBody hoverEffect={true} link="https://www.bp.com/">
+            <h3 className={styles["donor-names"]}>BP</h3>
+          </SectionBody>
+          <SectionBody hoverEffect={true} link="https://www.micron.com/">
+            <h3 className={styles["donor-names"]}>Micron</h3>
+          </SectionBody>
+          <SectionBody hoverEffect={true} link="https://usa.visa.com/">
+            <h3 className={styles["donor-names"]}>Visa</h3>
+          </SectionBody>
+          <SectionBody hoverEffect={true} link="https://www.zillow.com/">
+            <h3 className={styles["donor-names"]}>Zillow</h3>
+          </SectionBody>
           <h2 className={styles["sponsor-level"]}>Silver level $500+</h2>
-          <a
-            className={styles["sponsor-link"]}
-            href="https://www.accenture.com"
-            target="_blank"
-          >
-            <div className={styles["sponsor-button"]}>
-              <h3 className={styles["donor-names"]}>Accenture</h3>
-            </div>
-          </a>
+          <SectionBody hoverEffect={true} link="https://www.accenture.com">
+            <h3 className={styles["donor-names"]}>Accenture</h3>
+          </SectionBody>
           {/* <h2 className="sponsor-level">Bronze level</h2> */}
         </Container>
       </div>
