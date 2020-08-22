dd6859c3fc1751f23b9fa0cf1f075f6cf667f4cd
Polarpi <eugeniachen123@gmail.com>
2020-08-22 09:47:38 -0500

    make next app the main folder

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    plugins: [
-        require('autoprefixer')
-    ]
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          "postcss-flexbugs-fixes",
+          [
+            "postcss-preset-env",
+            {
+              autoprefixer: {
+                flexbox: "no-2009",
+              },
+              stage: 3,
+              features: {
+                "custom-properties": false,
+              },
+            },
+          ],
+        ]
+      : [
+          // No transformations in development
+        ],
 };
