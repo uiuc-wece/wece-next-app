99ffda76046a141e789a329b58ca6ef08117e57b
Polarpi <eugeniachen123@gmail.com>
2020-09-01 22:36:15 -0500

    add route for unsubscribing from MailerLite

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const addSubscriber = require("./mailerlite.js");
+const { addSubscriber, unsubscribe } = require("./mailerlite.js");
 
 router.post("/subscriber", addSubscriber);
+router.put("/subscriber", unsubscribe);
 
 module.exports = router;
