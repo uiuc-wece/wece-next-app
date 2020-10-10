833c9dbc335e25b88300fe34d01f7f4533b9473c
Ariya Patel <ariyapatel@wirelessprv-10-195-29-116.near.illinois.edu>
2020-10-10 16:20:51 -0500

    final_changes

--- pages/sponsors21.jsx
+++ pages/sponsors21.jsx
@@ -11,18 +11,11 @@ export default function Sponsors21() {
       <div className="content">
         <Container className="section">
           <SectionHead title="WECE Sponsor_" top={true} />
-          <SectionBody
-            hoverEffect={true}
-            link="https://www.microsoft.com/en-us/about"
-          >
-            <h1 className={styles["main-sponsor"]}>Microsoft</h1>
-          </SectionBody>
           <SectionHead title="Donors_" top={true} />
           <h2 className={styles["sponsor-level"]}>Platinum level $2000+</h2>
             <SectionBody hoverEffect={true} link="https://www.intel.com/">
             <h3 className={styles["donor-names"]}>Intel</h3>
           </SectionBody>
-          <h2 className={styles["sponsor-level"]}>Gold level $1000+</h2>
           <h2 className={styles["sponsor-level"]}>Bronze level</h2> 
           <SectionBody hoverEffect={true} link="https://www.capitalone.com/">
             <h3 className={styles["donor-names"]}>Capital One</h3>
