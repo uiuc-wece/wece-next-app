38e04b47be34e4598ed1dc86adda66b779dd5158
Polarpi <eugeniachen123@gmail.com>
2020-10-29 23:53:40 -0500

    reformat wece app with frontend in client and backend in api

---
+++ client/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/join":["static\u002Fchunks\u002Fpages\u002Fjoin.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  base_url: process.env.API_URL
-    ? process.env.API_URL
-    : "https://wece.ece.illinois.edu/api",
-};
