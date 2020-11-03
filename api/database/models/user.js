d5c04c3fbf6847461fe616c8f340d57cc63bdf25
Polarpi <eugeniachen123@gmail.com>
2020-11-03 16:57:00 -0600

    create user schema

---
+++ api/database/models/user.js
@@ -0,0 +1,30 @@
+// Load required packages
+import mongoose from "mongoose";
+
+// Define our user schema
+var UserSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String, required: true },
+  password: { type: String }, // user defined, encrypted
+  accountType: {
+    type: String,
+    enum: ["SPONSOR", "MEMBER", "BOARD", "CHAIR"],
+    required: true,
+  },
+  major: { type: String },
+  studentStatus: {
+    type: String,
+    enum: ["UNDERGRADUATE", "GRADUATE", "ALUMNI", "NONE"],
+  }, // undergrad, graduate, none
+  joinDate: { type: Date }, // join date to weceweb portal
+  graduationDate: { type: Date },
+  totalPoints: { type: Number },
+  eventsAttended: [{ type: String }], // list of event keys
+  committees: { type: String },
+  blogposts: { type: String },
+  profileImage: { data: Buffer, contentType: String },
+});
+
+// Export the Mongoose model
+export default mongoose.model("User", UserSchema);
