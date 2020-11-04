1dd5517f7bde900cb033c983011dd0f2ac64b5b9
Polarpi <eugeniachen123@gmail.com>
2020-11-04 10:29:35 -0600

    add user + blogpost crud function placeholders

---
+++ api/routes/user.js
@@ -0,0 +1,17 @@
+var User = require("../database/models/user.js");
+var mongoose = require("mongoose");
+
+function getUser(req, res, next) {}
+
+function deleteUser(req, res, next) {}
+
+function createUser(req, res, next) {}
+
+function updateUser(req, res, next) {}
+
+module.exports = {
+  getUser: getUser,
+  deleteUser: deleteUser,
+  createUser: createUser,
+  updateUser: updateUser,
+};
