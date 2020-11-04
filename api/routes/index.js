9f4dd3d54fb2964ae57f753d2af816cc7588cbb3
Polarpi <eugeniachen123@gmail.com>
2020-11-04 10:29:46 -0600

    add routes for user + blogpost

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const { addSubscriber, unsubscribe } = require("./mailerlite.js");
+const { getUser, deleteUser, createUser, updateUser } = require("./user.js");
+const {
+  getBlogpost,
+  deleteBlogpost,
+  createBlogpost,
+  updateBlogpost,
+} = require("./blogpost.js");
 
 router.post("/subscriber", addSubscriber);
 router.put("/subscriber", unsubscribe);
+
+router.get("/user/:id", getUser);
+router.delete("/user/:id", deleteUser);
+router.post("/user", createUser);
+router.put("/user/:id", updateUser);
+
+router.get("/blogpost/:id", getBlogpost);
+router.delete("/blogpost/:id", deleteBlogpost);
+router.post("/blogpost", createBlogpost);
+router.put("/blogpost/:id", updateBlogpost);
 
 module.exports = router;
