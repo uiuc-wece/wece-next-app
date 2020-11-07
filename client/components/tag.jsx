0cee847453d3e7335f8be1272ea4501f41273fc0
Nynika Badam <nynikabadam@gmail.com>
2020-11-07 12:34:37 -0600

    created tag styles file

---
+++ client/components/tag.jsx
@@ -0,0 +1,8 @@
+import styles from "../styles/tag.module.css";
+export default function tag() {
+    return (
+      <div className={styles["button"]}>
+          <a className={styles["link-btn"]}>{buttonText}</a>
+      </div>
+    );
+  }
