45affe7df9661bc17c0e34130ed1cd9147debbd6
Joshmita Chintala <joshmita.c31@gmail.com>
2020-11-16 21:29:04 -0600

    updated blogpost routes and endpoints

--- api/routes/blogpost.js
+++ api/routes/blogpost.js
@@ -1,13 +1,51 @@
 var Blogpost = require("../database/models/blogpost.js");
 var mongoose = require("mongoose");
 
-function getBlogpost(req, res, next) {}
+async function getBlogpost(req, res, next) 
+{
+  const posts = await Blogpost.find({});
 
-function deleteBlogpost(req, res, next) {}
+  try {
+    res.send(posts);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
 
-function createBlogpost(req, res, next) {}
+async function deleteBlogpost(req, res, next) 
+{
+  try {
+    const post = await Blogpost.findByIdAndDelete(req.params.id)
 
-function updateBlogpost(req, res, next) {}
+    if (!post) res.status(404).send("No item found")
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+async function createBlogpost(req, res, next) 
+{
+  const post = new Blogpost(req.body);
+
+  try {
+    await post.save();
+    res.send(post);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
+
+async function updateBlogpost(req, res, next) 
+{
+  try {
+    await Blogpost.findByIdAndUpdate(req.params.id, req.body)
+    await Blogpost.save()
+    res.send(post)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
 
 module.exports = {
   getBlogpost: getBlogpost,
