385832ee5d9c4df101c7cdeb18e16a521235134f
Polarpi <eugeniachen123@gmail.com>
2020-12-30 15:15:08 -0600

    update user schema

--- api/database/models/user.js
+++ api/database/models/user.js
@@ -1,30 +1,51 @@
 // Load required packages
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-// Define our user schema
-var UserSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  email: { type: String, required: true },
-  password: { type: String }, // user defined, encrypted
-  accountType: {
-    type: String,
-    enum: ["SPONSOR", "MEMBER", "BOARD", "CHAIR"],
-    required: true,
-  },
-  major: { type: String },
-  studentStatus: {
-    type: String,
-    enum: ["UNDERGRADUATE", "GRADUATE", "ALUMNI", "NONE"],
-  }, // undergrad, graduate, none
-  joinDate: { type: Date }, // join date to weceweb portal
-  graduationDate: { type: Date },
-  totalPoints: { type: Number },
-  eventsAttended: [String], // list of event keys
-  committees: [String],
-  blogposts: [String], // list of blogpost ids
-  profileImage: { data: Buffer, contentType: String },
+const accountTypeEnum = {
+  SPONSOR: "SPONSOR",
+  MEMBER: "MEMBER",
+  BOARD: "BOARD",
+  CHAIR: "CHAIR",
+  ADMIN: "ADMIN",
+};
+
+const ThirdPartyProviderSchema = new mongoose.Schema({
+  providerName: { type: String, defualt: null },
+  providerId: { type: String, default: null },
+  providerData: { type: {}, default: null },
 });
 
+// Define our user schema
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String, required: true },
+    emailIsVerified: { type: Boolean, default: false },
+    password: { type: String }, // user defined, encrypted
+    thirdPartyAuth: [ThirdPartyProviderSchema],
+    accountType: {
+      type: String,
+      enum: ["SPONSOR", "MEMBER", "BOARD", "CHAIR", "ADMIN"],
+      required: true,
+    },
+    major: { type: String },
+    studentStatus: {
+      type: String,
+      enum: ["UNDERGRADUATE", "GRADUATE", "ALUMNI", "NONE"],
+    }, // undergrad, graduate, none
+    joinDate: { type: Date }, // join date to weceweb portal
+    graduationDate: { type: Date },
+    totalPoints: { type: Number },
+    eventsAttended: [String], // list of event keys
+    committees: [String],
+    blogposts: [String], // list of blogpost ids
+    profileImage: { data: Buffer, contentType: String },
+  },
+  { strict: false }
+);
+
 // Export the Mongoose model
-export default mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
+module.exports.accountTypeEnum = accountTypeEnum;
