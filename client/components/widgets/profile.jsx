6529731e6cf301f838164d190758757791c4cac8
Polarpi <eugeniachen123@gmail.com>
2021-01-01 15:36:39 -0600

    add image upload to profile widget

--- client/components/widgets/profile.jsx
+++ client/components/widgets/profile.jsx
@@ -2,10 +2,22 @@ import AccountWidget from "../accountwidget";
 import styles from "../../styles/Profilewidget.module.css";
 
 import Image from "react-bootstrap/Image";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { BiCamera } from "react-icons/bi";
 
+import { useState, useRef } from "react";
 import { useSelector } from "react-redux";
+import axios from "axios";
+
+import { base_url } from "../../constants.js";
+
+import { refreshAuth } from "../../apihelper.js";
 
 const ProfileWidget = () => {
+  const profileImageRef = useRef();
+  const [modal, setModal] = useState(false);
   const {
     email,
     firstName,
@@ -16,6 +28,32 @@ const ProfileWidget = () => {
     graduationDate,
     profileImage,
   } = useSelector((state) => state);
+
+  const toggle = () => setModal(!modal);
+
+  const handleSubmit = (event) => {
+    const updateUrl = base_url + "/user";
+    if (profileImageRef.current.files[0]) {
+      const formData = new FormData();
+      formData.append("profileImage", profileImageRef.current.files[0]);
+      axios
+        .put(updateUrl, formData, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        })
+        .then(() => {
+          refreshAuth();
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+        })
+        .finally(() => {
+          toggle();
+        });
+    }
+  };
 
   const education = () => {
     if (studentStatus != "NONE" && studentStatus && major) {
@@ -63,11 +101,12 @@ const ProfileWidget = () => {
     <AccountWidget>
       <div className={styles["profile-info"]}>
         <div className={styles["profile-left"]}>
-          <Image
-            className={styles["profile-image"]}
-            src={profileImage}
-            roundedCircle
-          />
+          <div className={styles["profile-image"]} onClick={toggle}>
+            <div className={styles["camera"]}>
+              <BiCamera />
+            </div>
+            <Image src={profileImage} roundedCircle />
+          </div>
           <div className={styles["name"]}>{`${firstName} ${lastName}`}</div>
           <div className={styles["email"]}>{email}</div>
           <div className={styles["account-type"]}>{accountType}</div>
@@ -82,6 +121,30 @@ const ProfileWidget = () => {
           </div>
         </div>
       </div>
+      <Modal show={modal} onHide={toggle}>
+        <Modal.Header className={styles["image-modal-header"]} closeButton>
+          Upload new profile picture
+        </Modal.Header>
+        <Modal.Body className={styles["image-modal"]}>
+          <Form name="picture-form">
+            <Form.Group>
+              <Form.Control
+                className={styles["modal-input"]}
+                type="file"
+                name="image"
+                ref={profileImageRef}
+              />
+              <Button
+                id={styles["submit-form"]}
+                type="button"
+                onClick={handleSubmit}
+              >
+                Upload
+              </Button>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </AccountWidget>
   );
 };
