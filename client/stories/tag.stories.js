2f94a5937b536dbbe7d12a44959762ee20aab8c4
nbadam2 <nbadam2@illinois.edu>
2021-01-07 18:03:17 -0600

    style changes

--- client/stories/tag.stories.js
+++ client/stories/tag.stories.js
@@ -18,3 +18,28 @@ Academic.args = {
   word: "Academic"
 }
 
+export const Marketing = Template.bind({});
+Marketing.args = {
+  word: "Marketing"
+}
+
+export const Mentorship = Template.bind({});
+Mentorship.args = {
+  word: "Mentorship"
+}
+
+export const Outreach = Template.bind({});
+Outreach.args = {
+  word: "Outreach"
+}
+
+export const Social = Template.bind({});
+Social.args = {
+  word: "Social"
+}
+
+export const Technical = Template.bind({});
+Technical.args = {
+  word: "Technical"
+}
+
