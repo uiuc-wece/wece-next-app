b5d9f66fd321922948891f2fb4d2b533aa5adb30
nynikabadam <nbadam2@illinois.edu>
2021-03-01 19:23:02 -0600

    finished list of tags

--- client/components/listOfTags.jsx
+++ client/components/listOfTags.jsx
@@ -1,6 +1,15 @@
 import styles from "../styles/listOfTags.module.css";
 import React from "react";
+import Tag from "./tag";
+import listOfTagsStories from "../stories/listOfTags.stories";
 
-export default function listOfTags() {
-  ;
+export default function ListOfTags({words}) {
+  return(
+    <div className={styles["list_style"]}>
+      {
+        words.map(word => <Tag word={word}> </Tag>)
+      }
+    </div>
+  );
+  
 }
