1657fbf8f0193e0a385d9d1b6644b04164ce96c4
nynikabadam <nbadam2@illinois.edu>
2021-03-13 12:46:25 -0600

    fixing changes requested

--- client/components/tag.jsx
+++ client/components/tag.jsx
@@ -2,6 +2,6 @@ import styles from "../styles/tag.module.css";
 import React from "react";
 export default function Tag({word}) {
     return (
-          <a className={styles["tag_style"]}>{word}</a>
+          <a className={styles["tag"]}> {word} </a>
     );
   }
