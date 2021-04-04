d4b0a0e59114495bd982b69f21f0083214733581
nynikabadam <nbadam2@illinois.edu>
2021-04-04 02:21:23 -0500

    started adding to component

--- client/components/recentposts.jsx
+++ client/components/recentposts.jsx
@@ -1,5 +1,14 @@
 import styles from ".../styles/Recentposts.module.css";
+import React from "react";
 
-export default function Recentposts() {
-    return ();
+export default function RecentPosts({title, post1, post2, post3}) {
+    return (
+        <RecentTop>
+          <div className={styles["recent-body"]}>
+            <div className={styles["recent-title"]}>
+              <h3>{title}</h3>
+            </div>
+          </div>
+        </RecentTop>
+      );
 }
