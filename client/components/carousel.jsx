f72877282c45519c420320b4daaf89b46e8d252b
Sujin Shin <shin369123@gmail.com>
2022-02-10 17:50:52 -0600

    Added new photos for home page

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -4,7 +4,7 @@ import Carousel from "react-bootstrap/Carousel";
 export default function EventCarousel() {
   return (
     <Carousel>
-      <Carousel.Item>
+      {/* <Carousel.Item>
         <img
           className="d-block w-100"
           src="imgs/events/mentorship/mentorship-craft-night-2019.JPG"
@@ -31,12 +31,61 @@ export default function EventCarousel() {
           src="imgs/events/infrastructure/html&cssworkshop.jpg"
           alt="html and css workshop"
         />
+      </Carousel.Item> */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="imgs/events/quad_day/IMG_4955.JPG"
+          alt="quad day 2021"
+        />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="imgs/events/quad_day/IMG_4955.jpg"
-          alt="quad day 2021"
+          src="imgs/events/GM/august-21.jpg"
+          alt="August 2021 General Meeting"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="imgs/events/retreat/retreat-group-photo-2.jpg"
+          alt="Retreat in Kickapoo Park"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="imgs/events/retreat/retreat-kayak.JPG"
+          alt="Kanooing in Kickapoo Park"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="imgs/events/icecream_social/icecream-social-2.jpg"
+          alt="Group photo at the ice cream social"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="imgs/events/icecream_social/icecream-social-1.jpg"
+          alt="Ice cream social at Scott Park"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="imgs/events/mentorship/relaxing-with-ross-2021-1.JPG"
+          alt="Bob Ross Painting  2021"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="imgs/events/mentorship/relaxing-with-ross-2021-2.JPG"
+          alt="Bob Ross Painting  2021"
         />
       </Carousel.Item>
     </Carousel>
