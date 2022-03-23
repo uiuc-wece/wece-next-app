02047b81ae25284d34288dd76fff0fa4b9c0bc04
Joshmita <jchint2@illinois.edu>
2022-03-23 14:20:15 -0500

    updated color scheme for board page and updated events page with a new format. added members resources pages

---
+++ client/pages/newsletters.jsx
@@ -0,0 +1,134 @@
+import Layout from "../components/layout";
+import SectionBody from "../components/sectionbody";
+import SectionHead from "../components/sectionhead";
+
+import Container from "react-bootstrap/Container";
+
+export default function newsletters() {
+  return (
+    <Layout>
+      <div className="container">
+        <div className="content">
+          <Container fluid className="section">
+            <SectionHead title="Newsletters" top={true} />
+            <SectionBody>
+            <p>
+                Access past newsletters here:{" "}
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/b1p6v4y2w8" target="_blank">
+                  March 21, 2022
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/q1z5o3e0j5" target="_blank">
+                  March 7, 2022
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/q5u2a5w5y2" target="_blank">
+                  February 28, 2022
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/a6v6t3q9y6" target="_blank">
+                  February 21, 2022
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/f9n3h6a8s4" target="_blank">
+                  February 14, 2022
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/e4e3f6i8j4" target="_blank">
+                  February 8, 2022
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/h6k8r8f9l4" target="_blank">
+                  January 31, 2022
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/v7i6f7" target="_blank">
+                  December 5, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/x1k0b7" target="_blank">
+                  November 29, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/f8m2u3" target="_blank">
+                  November 15, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/i3o0t9" target="_blank">
+                    November 8, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/m2g6u4" target="_blank">
+                  November 1, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/a8y0o0" target="_blank">
+                  October 25, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/q7q1q0" target="_blank">
+                  October 19, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/n9t0d5" target="_blank">
+                  October 12, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/b8j1y9" target="_blank">
+                  October 4, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/j0e7e0" target="_blank">
+                  September 27, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/i6n4s4" target="_blank">
+                  September 20, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/v2p4k2" target="_blank">
+                  September 13, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/g7d4i3" target="_blank">
+                  September 6, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/e8c3c6" target="_blank">
+                  September 2, 2021
+                </a>
+            </p>
+            <p>
+                <a href="https://preview.mailerlite.com/e2m5q8" target="_blank">
+                  August 30, 2021
+                </a>
+            </p>
+            </SectionBody>
+          </Container>
+        </div>
+      </div>
+    </Layout>
+  );
+}
