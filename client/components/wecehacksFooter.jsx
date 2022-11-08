b19fedceed1b9162bea44a8d32cdb7c75dc402a4
estrella <epopo2@illinois.edu>
2022-11-07 20:05:38 -0600

    fix typo

--- client/components/wecehacksFooter.jsx
+++ client/components/wecehacksFooter.jsx
@@ -4,35 +4,38 @@ import styles from "../styles/Footer.module.css";
 
 export default function WECEHacksFooter() {
   return (
-    <div className={styles["wecehacks-footer"]} >
+    <div className={styles["wecehacks-footer"]}>
       <div className={styles["wecehacks-footer-content"]}>
-      <div className={styles["wecehacks-navbar-text-left"]}> 
-        <SocialIcon style={{
-            width: "4em",
-            height: "4em",
-          }} 
-          network="instagram"
-          url="https://www.instagram.com/wecehac.ks/"
-          fgColor="white"
-          bgColor="transparent" 
-        />
-        <SocialIcon style={{
-          width: "4em",
-          height: "4em",
-        }}
-        network="email"
-        url="mailto:wece.uiuc@gmail.com"
-        fgColor="white"
-        bgColor="transparent"
-        />
+        <div className={styles["wecehacks-navbar-text-left"]}>
+          <SocialIcon
+            style={{
+              width: "4em",
+              height: "4em",
+            }}
+            network="instagram"
+            url="https://www.instagram.com/wecehac.ks/"
+            fgColor="white"
+            bgColor="transparent"
+          />
+          <SocialIcon
+            style={{
+              width: "4em",
+              height: "4em",
+            }}
+            network="email"
+            url="mailto:wece.uiuc@gmail.com"
+            fgColor="white"
+            bgColor="transparent"
+          />
+        </div>
+        <div className={styles["wecehacks-navbar-text-right"]}>
+          <p>© WECEHacks Team 2023</p>
+          <p>
+            Women in Electrical and Computer Engineering <br />
+            <p>1016 ECEB, 306 North Wright Street, Urbana, IL 61801</p>
+          </p>
+        </div>
       </div>
-      <div className={styles["wecehacks-navbar-text-right"]}>
-        <p>© WECEHacks Team 2023</p>
-        <p>Women in Electrical and Computer Enginerring <br/>
-        <p>1016 ECEB, 306 North Wright Street, Urbana, IL 61801</p>
-        </p>
-        </div>
-        </div>
     </div>
   );
 }
