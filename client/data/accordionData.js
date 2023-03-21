2e1e5e1e5cf051969560db2e1424ad9e3a381fc4
amaigabichan <gabbypop07@msn.com>
2023-03-20 20:35:01 -0500

    faq update 2

--- client/data/accordionData.js
+++ client/data/accordionData.js
@@ -31,4 +31,32 @@ export const accordionData = [
     question: "Interested in hardware?",
     answer: "During the event, there will be workshops dedicated on learning how to install Arduino IDE on your PC, an intro to Raspberry Pi, and even learn how to use a motion sensor device! You can checkout hardware using your student ID.",
   },
+  {
+    question: "I am male and use he/him pronouns. Can I still participate?",
+    answer: "You must identify as a historically underrepresented gender to participate such as women, or those who identify as non binary. However, we do accept mentor applications through our email.",
+  },
+  {
+    question: "What do you mean by 'minority gender'?",
+    answer: "By "minority gender", we mean individuals who identify as women, non-binary, genderqueer, gender non-conforming, or any other gender identity that is not traditionally represented in the tech industry.",
+  },
+  {
+    question: "What kind of projects can we work on?",
+    answer: "You can work on any project that you believe will have a positive impact on the tech industry and/or underrepresented communities. We encourage you to tackle sponsor prizes, or a prize category to form an idea of what you can build!",
+  },
+  {
+    question:"How many people can be on a team?",
+    answer: "You can have up to 4 people on a team!",
+  },
+  {
+    question: "What if I don’t have a team?",
+    answer: "You'll have the chance to meet other hackers by joining our Discord server! We'll have a channel for you to find teammates!",
+  },
+  {
+    question:"What are the judging criteria?",
+    answer: "Projects will be judged on creativity, impact, and feasibility. We encourage teams to think about the potential impact of their project and how it can be scaled or implemented in the real world.",
+  },
+  {
+    question:"Will food and drinks be provided?",
+    answer:"Yes, we will provide food and drinks throughout the hackathon. If you have any dietary restrictions or preferences, please let us know in advance so that we can accommodate your needs.",
+  },
 ];
