de62c1e91d50f3226aa8945a408f5efd80ca7681
amaigabichan <gabbypop07@msn.com>
2023-08-30 19:17:41 -0500

    Update GM Slides Links

--- client/pages/gmslides.jsx
+++ client/pages/gmslides.jsx
@@ -12,33 +12,18 @@ export default function gmslides() {
           <Container fluid className="section">
             <SectionHead title="GM Slides" top={true} />
             <SectionBody>
-            <p>
+              <p>
                 Access past General Meeting slides here:{" "}
-            </p>
-            <p>
-                <a href="https://docs.google.com/presentation/d/14cXtMZxfbV3bsjZH-7BnLMZ1C7IfwrRNue7dn9S37JY/edit?usp=sharing" target="_blank">
-                  February
+              </p>
+              <p>
+                <a href="https://docs.google.com/presentation/d/11MD2HiB8lX89Yom06Pmb-qnt3z2i1LLv06dnFcnIB3s/edit?usp=sharing" target="_blank">
+                  New Slides
                 </a>
-            </p>
-            <p>
-                <a href="https://docs.google.com/presentation/d/1wULHWFUNW7JSa5GSx9S__Duft2_T_xeZcw9pxooh5gQ/edit?usp=sharing" target="_blank">
-                  December
-                </a>
-            </p>
-            <p>
-                <a href="https://docs.google.com/presentation/d/17ZRVUvHA3b7W9v-Y9YMLG7wUK7i4X2RXRpqWjYrLq48/edit?usp=sharing" target="_blank">
-                  October
-                </a>
-            </p>
-            <p>
-                <a href="https://docs.google.com/presentation/d/1C5Y2_xcKfFlSjMwP4QeNYdYb6EjtFqyd112_MJgesww/edit#slide=id.p" target="_blank">
-                  August
-                </a>
-            </p>
+              </p>
             </SectionBody>
           </Container>
         </div>
       </div>
     </Layout>
-  );
+  );  
 }
