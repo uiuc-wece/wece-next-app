c93fdc11c49fdf5636ca200aa9644a0f347bc86a
amaigabichan <gabbypop07@msn.com>
2023-09-25 02:46:50 -0500

    sponsor images upd

--- client/pages/sponsors21.jsx
+++ client/pages/sponsors21.jsx
@@ -2,25 +2,44 @@ import Layout from "../components/layout";
 import SectionBody from "../components/sectionbody";
 import SectionHead from "../components/sectionhead";
 import styles from "../styles/Sponsors.module.css";
-
 import Container from "react-bootstrap/Container";
 
-export default function Sponsors21() {
+export default function Sponsors() {
   return (
     <Layout>
       <div className="content">
         <Container className="section">
-          <SectionHead title="Sponsors_" top={true} />
+          <SectionHead title="WECE Sponsors_" top={true} />
+
           <h2 className={styles["sponsor-level"]}>Platinum level $2000+</h2>
-            <SectionBody hoverEffect={true} link="https://www.intel.com/">
-            <h3 className={styles["donor-names"]}>Intel</h3>
-          </SectionBody>
           <SectionBody hoverEffect={true} link="https://www.synchrony.com/">
-            <h3 className={styles["donor-names"]}>Synchrony</h3>
+            <div>
+              <img
+                className={styles["sponsor-img"]}
+                src="../imgs/synchrony-logo.png"
+                alt="Synchrony Logo"
+              />
+            </div>
           </SectionBody>
-          <h2 className={styles["sponsor-level"]}>Bronze level</h2> 
+          <SectionBody hoverEffect={true} link="https://www.intel.com/">
+            <div>
+              <img
+                className={styles["sponsor-img"]}
+                src="../imgs/intel.png"
+                alt="Intel Logo"
+              />
+            </div>
+          </SectionBody>
+
+          <h2 className={styles["sponsor-level"]}>Bronze level </h2>
           <SectionBody hoverEffect={true} link="https://www.capitalone.com/">
-            <h3 className={styles["donor-names"]}>Capital One</h3>
+            <div>
+              <img
+                className={styles["sponsor-img"]}
+                src="../imgs/capitalone.png"
+                alt="Capital One Logo"
+              />
+            </div>
           </SectionBody>
         </Container>
       </div>
