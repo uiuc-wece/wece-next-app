ffe356fc7ed18f31957955346a4f9d1c931dacf0
amaigabichan <gabbypop07@msn.com>
2024-01-27 19:33:02 -0600

    sposor 23-24

--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -113,6 +113,12 @@ const CustomNavbar = ({ background_color2 = "#9D9DFF" }) => {
 
             <NavDropdown title="Sponsors">
               <NavDropdown.Item
+               className={styles["dropdown-item"]}
+               href="/sponsors24"
+              >
+                2023- 24
+              </NavDropdown.Item>
+              <NavDropdown.Item
                 className={styles["dropdown-item"]}
                 href="/sponsors23"
               >
